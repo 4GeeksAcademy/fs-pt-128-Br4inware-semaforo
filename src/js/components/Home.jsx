4dd2602c4f778b3c26493937d40ded8276a7abc0
Br4inware <bsabusiness@outlook.com>
2025-12-17 19:38:43 +0000

    basics requeriments of excercise doit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+const [encendido, setEncendido] = useState("")
+
+	return (
+		
+		<div className="semaforo row colum ">
+			<div className={`red ${encendido == "red" ? "light" : ""}`} onClick={()=> setEncendido("red")} >
+			</div>
+			<div className={`yellow ${encendido == "yellow" ? "light" : ""}`} onClick={()=> setEncendido("yellow")} >
+			</div>
+			<div className={`green ${encendido == "green" ? "light" : ""}`} onClick={()=> setEncendido("green")} >
+			</div>
 		</div>
 	);
 };
